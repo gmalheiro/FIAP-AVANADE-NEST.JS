6d1ab139c5db5851679732894a6484aa2c44519d
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2022-10-30 16:43:14 -0300

    Update on Users service, controller and modules| The project is creating a row in the database

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,39 +1,33 @@
-import {
-  Body,
-  Controller,
-  Post,
-  Get,
-  Param,
-  ParseUUIDPipe,
-  Patch,
-} from '@nestjs/common';
-import { UsersService } from './users.service';
+import { Body, Controller, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
 import { CreateUserDTO } from './dto/createUser.dto';
 import { UpdateUserDTO } from './dto/updateUser.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService){}
 
-  // criar
-  @Post()
-  create(@Body() req: CreateUserDTO) {
-    return this.usersService.create();
-  }
-  // listar todos  localhost:3000/users
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
-  // listar um  localhost:3000/users/1
-  @Get(':id')
-  findOne(@Param('id', ParseUUIDPipe) id: number) {
-    return this.usersService.findOne(id);
-  }
-  // atualizar
-  @Patch(':id')
-  update(@Param('id', ParseUUIDPipe) id: number, @Body() req: UpdateUserDTO) {
-    return this.usersService.update(id, req);
-  }
-  // deletar
+    // criar
+    @Post()
+    create(@Body() req: CreateUserDTO) {
+        return this.usersService.create(req);
+    }
+    // listar todos http://localhost:3000/users
+    @Get()
+    findAll(){
+        return this.usersService.findAll();
+    }
+    // listar um  http://localhost:3000/users/1
+    @Get(':id')
+    findOne(@Param('id', ParseUUIDPipe) id: number){
+        return this.usersService.findOne(+id);
+    }
+    // atualizar
+    @Patch(':id')
+    update(
+        @Param('id', ParseUUIDPipe) id: number,
+        @Body() req: UpdateUserDTO){
+            return this.usersService.update(id, req);
+        }
+    // deletar
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import {UsersController} from './users.controller';
 
 @Module({
-  providers: [UsersService],
-  controllers: [UsersController]
+  controllers: [UsersController],
+  providers: [UsersService, PrismaService]
 })
 export class UsersModule {}
